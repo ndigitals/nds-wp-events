fe45af21183355c48119700b3c2b53c25a984829
timnolte <9O4nMQtXHs2O>
2014-06-11 23:25:18 -0400

    Updated the jQuery UI Google CDN reference to be over SSL.

--- inc/admin/class-admin.php
+++ inc/admin/class-admin.php
@@ -439,7 +439,7 @@ CSS;
         wp_enqueue_script( 'jquery-ui-datepicker' );
         wp_enqueue_style(
             'jquery-style',
-            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/themes/smoothness/jquery-ui.css'
+            'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/themes/smoothness/jquery-ui.css'
         );
 
         // - grab wp time format -
